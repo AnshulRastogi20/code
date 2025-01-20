83792ba1b11b8faa08646e532a0012e410b4769e
AnshulRastogi20 <anshulrastogi020@gmai.com>
2025-01-20 16:00:53 +0530

    fixed darkmagenta

--- src/components/ui/material/Hero.tsx
+++ src/components/ui/material/Hero.tsx
@@ -141,7 +141,7 @@ export default function Hero() {
             /> */}
             <Button
               variant="contained"
-              color="darkmagenta"
+              color="success"
               onClick={() => {
 
                 if(session?.user) {
